3d5d8fc302d04a36ee6e568c82e5889ba3920fb9
roshni kashyap <roshni.kashyap@tothenew.com>
2019-05-02 16:02:38 +0530

    ADD:: Middleware and configure store as per that

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import configureStore from './store';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Route } from 'react-router-dom';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
+
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
-import PRList from './components/PrListing';
+import rootReducer from './reducers/rootReducer';
+import { fetchAllPRs } from './actions/prListAction';
+import PRList from './containers/PrListContainer';
 import AddPR from './components/AddPR';
 
+import * as serviceWorker from './serviceWorker';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+console.log(store, 'store');
+store.dispatch(fetchAllPRs());
+
 ReactDOM.render(
- <Provider store={configureStore()}>
-  <Router>
-      <Route path='/' component={App} />
-      <Route path='/view-pr' component={PRList} />
-      <Route path='/add-pr' component={AddPR} />
-  </Router>
- </Provider>,
- document.getElementById('root'),
-);
+    <Provider store={store}>
+        <Router>
+        <Route path='/' component={App} />
+        <Route path='/view-pr' component={PRList} />
+        <Route path='/add-pr' component={AddPR} />
+        </Router>
+    </Provider>, document.getElementById('root'));
 
 serviceWorker.register();
